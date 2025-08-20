9a166a543c7bde918d8db3912c925c115b711be7
Tri Sutrisno <mas.trisutrisno@gmail.com>
2025-08-20 18:28:21 +0700

    add user places fetching logic with error handling and loading state

--- src/app/places/user-places/user-places.component.ts
+++ src/app/places/user-places/user-places.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { PlacesComponent } from '../places.component';
+import { Place } from '../place.model';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-user-places',
@@ -10,5 +13,40 @@ import { PlacesComponent } from '../places.component';
   styleUrl: './user-places.component.css',
   imports: [PlacesContainerComponent, PlacesComponent],
 })
-export class UserPlacesComponent {
+export class UserPlacesComponent implements OnInit {
+
+    isFetching = signal(false);
+    places = signal<Place[] | undefined>(undefined);
+    error = signal<string | null>(null);
+    private readonly httpClient = inject(HttpClient);
+    private readonly destroyRef = inject(DestroyRef);
+
+    ngOnInit() {
+      this.isFetching.set(true);
+      const subscription = this.httpClient
+        .get<{ places: Place[] }>('http://localhost:3000/user-places')
+        .pipe(
+          map((resData) => resData.places),
+          catchError((error) => {
+            return throwError(
+              () => new Error('Failed to fetch places your favorite places. Please try again later')
+            );
+          })
+        )
+        .subscribe({
+          next: (places) => {
+            this.places.set(places || []);
+          },
+          error: (err: Error) => {
+            this.error.set(err.message);
+          },
+          complete: () => {
+            this.isFetching.set(false);
+          },
+        });
+  
+      this.destroyRef.onDestroy(() => {
+        subscription.unsubscribe();
+      });
+    }
 }
