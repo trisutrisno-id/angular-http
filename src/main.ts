a8e192d00a03da8ccd078dffc3f0921c0368aacd
Tri Sutrisno <mas.trisutrisno@gmail.com>
2025-08-20 22:40:20 +0700

    feat: add logging interceptor for HTTP requests

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,27 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
+import {
+  HttpHandlerFn,
+  HttpRequest,
+  provideHttpClient,
+  withInterceptors,
+} from '@angular/common/http';
 
 import { AppComponent } from './app/app.component';
+ 
+function loggingInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn) {
+  console.log('Request made:', req);
 
+//   const reqWithHeaders = req.clone({
+//     headers: req.headers.set('Custom-Header', 'CustomValue'),
+//   });
+
+  return next(req);
+}
 
 bootstrapApplication(AppComponent, {
-    providers: [provideHttpClient()]
+  providers: [
+    provideHttpClient(
+      withInterceptors([loggingInterceptor]) // Add your interceptors here if needed
+    ),
+  ],
 }).catch((err) => console.error(err));
