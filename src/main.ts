d97fc31a853f0284af2e28a874c894a9f5bc6f57
Tri Sutrisno <mas.trisutrisno@gmail.com>
2025-08-20 22:45:32 +0700

    feat: enhance logging interceptor to handle HTTP responses and errors

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import {
+  HttpEventType,
   HttpHandlerFn,
   HttpRequest,
   provideHttpClient,
@@ -7,15 +8,29 @@ import {
 } from '@angular/common/http';
 
 import { AppComponent } from './app/app.component';
- 
+import { tap } from 'rxjs';
+
 function loggingInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn) {
   console.log('Request made:', req);
 
-//   const reqWithHeaders = req.clone({
-//     headers: req.headers.set('Custom-Header', 'CustomValue'),
-//   });
+  //   const reqWithHeaders = req.clone({
+  //     headers: req.headers.set('Custom-Header', 'CustomValue'),
+  //   });
 
-  return next(req);
+  return next(req).pipe(
+    tap({
+      next: (event) => {
+        if (event.type === HttpEventType.Response) {
+          console.log('[Incoming Response]');
+          console.log('Response received:', event.status);
+          console.log('Response received:', event.body);
+        }
+      },
+      error: (error) => {
+        console.error('Error occurred:', error);
+      },
+    })
+  );
 }
 
 bootstrapApplication(AppComponent, {
