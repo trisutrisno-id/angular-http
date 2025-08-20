408b3d4172d7697fd61712f1e3daeb748bcbb26c
Tri Sutrisno <mas.trisutrisno@gmail.com>
2025-08-20 13:52:06 +0700

    fetch data using angular HttpClient
    - import provideHttpClient in main

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -1,5 +1,5 @@
-import { Component, signal } from '@angular/core';
-
+import { Component, signal, inject, OnInit, DestroyRef } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
@@ -11,6 +11,26 @@ import { PlacesContainerComponent } from '../places-container/places-container.c
   styleUrl: './available-places.component.css',
   imports: [PlacesComponent, PlacesContainerComponent],
 })
-export class AvailablePlacesComponent {
+export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
+  private readonly httpClient = inject(HttpClient);
+  private readonly destroyRef = inject(DestroyRef);
+
+  // constructor(private http: HttpClient) {}} 
+
+  ngOnInit(){
+    const subscription = this.httpClient.get<{places: Place[]}>('http://localhost:3000/places').subscribe({
+      next: (resData) => {
+        console.log("🚀 ~ AvailablePlacesComponent ~ ngOnInit ~ places:", resData.places)
+      },
+      error: (err) => {
+        console.error('Error fetching places:', err);
+      }
+    });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
+
 }
