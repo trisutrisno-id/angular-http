400272ccb7c3c1329ba171966fbedb26a13a54fc
Tri Sutrisno <mas.trisutrisno@gmail.com>
2025-08-20 14:00:32 +0700

    fetch and transform using RxJs map and render to ui

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -19,9 +20,13 @@ export class AvailablePlacesComponent implements OnInit {
   // constructor(private http: HttpClient) {}} 
 
   ngOnInit(){
-    const subscription = this.httpClient.get<{places: Place[]}>('http://localhost:3000/places').subscribe({
-      next: (resData) => {
-        console.log("🚀 ~ AvailablePlacesComponent ~ ngOnInit ~ places:", resData.places)
+    const subscription = this.httpClient.get<{places: Place[]}>('http://localhost:3000/places')  
+    .pipe( 
+      map((resData) => resData.places)
+    )
+    .subscribe({
+      next: (places) => {
+        this.places.set(places || []);
       },
       error: (err) => {
         console.error('Error fetching places:', err);
